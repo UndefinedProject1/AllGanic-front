43185aa9f669443b9b37d954d82a57a5214fd111
kaydan95 <kyori0515@gmail.com>
2021-10-27 17:32:58 +0900

    20211027_수정
    - 관리자 페이지 (admin_page)
    : 사이드 메뉴 구현

--- src/components/Admin_Page.vue
+++ src/components/Admin_Page.vue
@@ -1,7 +1,29 @@
 <template>
     <div class="admin_wrapper">
-        <div class="admin_container">
-            admin test
+        <div class="admin_sidmenu">
+            <p>Admin</p>
+            <div class="divider"></div>
+            <div class="sidemenu_container">
+                <div class="section">
+                    <h2>브랜드관리</h2>
+                    <p>브랜드 추가</p>
+                    <p>브랜드 등록현황</p>
+                </div>
+                <div class="section">
+                    <h2>상품관리</h2>
+                    <p>제품 등록</p>
+                    <p>제품 등록 현황</p>
+                </div>
+                <div class="section">
+                    <h2>통계</h2>
+                    <p>미정</p>
+                    <p>미정</p>
+                </div>
+                <h2 class="last_section">QA관리</h2>
+            </div>
+        </div>
+        <div class="admin_content">
+            admin content
         </div>
     </div>
 </template>
@@ -13,6 +35,8 @@
 </script>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Gowun+Batang:wght@700&family=Gowun+Dodum&family=Playfair+Display:wght@400;500;700;800&display=swap');
+@import url('https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@700&display=swap');
 .admin_wrapper {
     background-color: white;
     z-index: 1000000;
@@ -20,5 +44,73 @@
     width: 100%;
     position: absolute;
     top: 0;
+
+    display: grid;
+    grid-template-rows: 100%;
+    grid-template-columns: 15% 85%;
+    grid-template-areas: "admin_sidmenu", "admin_content";
+    height: 100vh;
+    font-family: 'Gowun Dodum', sans-serif;
 }
+.admin_sidmenu{
+    border: 1px solid black;
+    background-color: #49654E;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.admin_sidmenu p {
+    /* border : 1px solid black; */
+    height: fit-content;
+    width : fit-content;
+    font-size: 30px;
+    font-weight: bold;
+    color: white;
+    margin : 30px auto;
+    font-family: 'Gowun Dodum', sans-serif;
+}
+.divider{
+    border : 0.3px solid #ffffff;
+    height: 0.3px;
+    width : 100%;
+}
+.sidemenu_container{
+    /* border : 1px solid black; */
+    display : flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 30px;
+}
+.sidemenu_container .section{
+    /* border : 1px solid black; */
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin : 20px;
+}
+.sidemenu_container h2{
+    font-size: 20px;
+    width: fit-content;
+    color: #ffffff;
+    font-weight: bold;
+    font-family: 'Nanum Gothic', sans-serif;
+}
+
+.sidemenu_container p{
+    font-size: 15px;
+    color : #ffffffef;
+    font-family: 'Gowun Dodum', sans-serif;
+    margin : 5px;
+}
+.sidemenu_container p:hover{
+    cursor: pointer;
+    color: antiquewhite;
+}
+.last_section{
+    margin-top: 10px;
+}
+.admin_content{
+    border: 1px solid black;
+}
+
 </style>
