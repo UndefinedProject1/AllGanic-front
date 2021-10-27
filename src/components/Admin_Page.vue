5d61915c0f4bbca690d404bee41e7084999ac39c
kaydan95 <kyori0515@gmail.com>
2021-10-27 15:57:05 +0900

    20211027_수정
    - 관리자페이지(admin_page) 추가가

---
+++ src/components/Admin_Page.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="admin_wrapper">
+        <div class="admin_container">
+            admin test
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+.admin_wrapper {
+    background-color: white;
+    z-index: 1000000;
+    height: 100% !important;
+    width: 100%;
+    position: absolute;
+    top: 0;
+}
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import Main from '@/components/Main.vue';
 import Join from '@/components/Join.vue';
 import Login from '@/components/Login.vue';
 import Cart_Popup from '@/components/Cart_Popup.vue';
+import Admin_Page from '@/components/Admin_Page.vue';
 
 
 const routes = [
@@ -11,6 +12,7 @@ const routes = [
     {path : '/join' , name :'join', component : Join},
     {path : '/login' , name :'login', component : Login},
     {path : '/cart_popup' , name :'cart_popup', component : Cart_Popup},
+    {path : '/admin_page' , name :'admin_page', component : Admin_Page},
 ];
 
 const router = createRouter({
