0c26decfa4c34dfed730b46081b24ee8fc9cc45f
kaydan95 <kyori0515@gmail.com>
2021-10-28 20:49:58 +0900

    Update Admin_Product_Insert.vue

--- src/components/Admin_Product_Insert.vue
+++ src/components/Admin_Product_Insert.vue
@@ -63,23 +63,55 @@
                 </div>
             </div>
             <div class="section2">
-                <div class="title_area">
-                    <span class="th_title">상세 이미지</span>
+                <div class="title_area2" style="padding-top:-10px">
+                    <span class="th_title" >상세 이미지</span>
                 </div>
                 <div class="content_area">
                     <div class="subimage_container">
+                        <div v-for="num in 3" v-bind:key="num">
+                            <img :src = "imageupload" class="subimage"> 
+                        </div>
                         
-                    </div>  
-                </div> 
+                    </div>
+                    <input type="file" @change="handleSubImg($event)" name="파일첨부" multiple>
+                </div>
             </div>
-            <button type="button"></button>
+            <button type="button">등록하기</button>
         </div>
     </div>
 </template>
 
 <script>
     export default {
-        
+        data() {
+            return {
+                imageupload : [],
+            }
+        },
+        methods: {
+            handleSubImg(e){
+                if(e.target.files.length > 0) {
+                    const fileArr = Array.from(e.target.files);
+
+                    fileArr.forEach(file => {
+                        const reader = new FileReader();
+
+                        reader.onload = e => {
+                            this.imageupload = e.target.result;
+                            console.log(file.name);
+                        }
+
+                        reader.readAsDataURL(file)
+                    })
+                    
+                    // var reader = new FileReader();
+                    // reader.onload = (e) => {
+                    //     this.imageupload = e.target.result;
+                    // }
+                    // reader.readAsDataURL(e.target.files[0]);
+                }
+            }
+        }
     }
 </script>
 
@@ -89,14 +121,14 @@
 .apl_wrapper {
     height: 100%;
     display : grid;
-    grid-template-rows: 14% 1% 85%;
+    grid-template-rows: 13.4% 1% 85%;
     grid-template-columns: 100%;
     grid-template-areas: "apl_header", "apl_divider" "apl_content";
     font-family: 'Exo', sans-serif;
 }
 .apl_header{
     width: 100%;
-    border: 1px solid black;
+    /* border: 1px solid black; */
     display: flex;
     align-items: center;
     justify-content: flex-start;
@@ -104,14 +136,20 @@
 }
 .apl_header p, span {
     width : fit-content;
-    margin : 0 0px 0px 20px;
+    margin : 0 0px 0px 40px;
     color : rgba(29, 29, 29, 0.795);
     font-size: 20px;
+    font-family: 'Exo', sans-serif;
+    font-weight : 400;
+}
+.apl_header > span {
+    margin-left : 20px;
 }
 #chosen {
-    font-family: 'Gowun Dodum', sans-serif;
-    font-weight: bold;
+    font-family: 'Exo', sans-serif;
+    font-weight: 700;
     color: black;
+    margin : 0 0px 0px 20px;
 }
 .apl_divider{
     border : 0.3px solid black;
@@ -121,26 +159,26 @@
 
 tbody{
     width : 300px;
-    border : 1px solid black;
+    /* border : 1px solid black; */
 
 }
 .apl_content{
     width: 100%;
-    height : 100%;
-    border:1px solid black;
+    height : fit-content;
+    /* border : 1px solid black; */
     display : flex;
     flex-direction: column;
     padding : 30px;
 }
 .section {
-    border: 1px solid black;
+    /* border : 1px solid black; */
     width: 100%;
     height: 50%;
     display: inline-flex;
 }
 
 .left_section {
-    border: 1px solid black;
+    /* border: 1px solid black; */
     width: 60%;
 }
 .tr{
@@ -149,12 +187,12 @@ tbody{
 .th_area{
     width : 200px;
     height : 100%;
-    border : 1px solid black;
+    /* border : 1px solid black; */
 }
 th{
     text-align: right;
     margin: 10px;
-    border : 1px solid black;
+    /* border : 1px solid black; */
 }
 .th_title{
     margin-right: 10px;
@@ -170,42 +208,50 @@ th{
 .td { 
     margin-bottom: 10px;
 }
-.form-select, .form-control{
+.form-select, .form-control {
     width : 90%;
-    margin : 0 auto;
     margin : 10px auto;
 }
 
 .right_section {
     border: 1px solid black;
-    width : 40%;
-    height: 100%;
+    width : 25%;
+    height: 250px;
 }
 
 
 
 .section2 {
-    border: 1px solid black;
+    /* border: 1px solid black; */
     width: 100%;
     height: 50%;
     display: flex;
     flex-direction: row;
+    margin-top : 15px;
 }
 .title_area{
     width : 17.5%;
     height : 100%;
-    border : 1px solid black;
+    /* border : 1px solid black; */
     text-align : right;
     padding-top : 10px;
+}
+.title_area2{
+    width : 17.5%;
+    height : 100%;
+    /* border : 1px solid black; */
+    text-align : right;
+    margin-top : -11px;
 }
 .content_area{
     width : 83%;
     height : 100%;
-    border : 1px solid black;
+    /* border : 1px solid black; */
 }
 #floatingTextarea2{
+    width : 80%;
     height: 235px;
-    margin : 13px 0px 13px 20px;
+    margin : 13px 0px 32px 20px;
 }
 .form-floating>label {
     color : rgba(37, 37, 37, 0.747);
@@ -220,22 +266,35 @@ th{
     transition: opacity .1s ease-in-out,transform .1s ease-in-out;
 }
 .content_area > p{
-    font-size: 15px;
+    font-size: 13px;
     font-family: 'Exo', sans-serif;
     color : rgba(37, 37, 37, 0.747);
     font-weight: 300;
-    margin-right: 30px;
+    margin-right: 60px;
+    margin-top : 30px;
 }
 .subimage_container{
-    border: 1px solid black;
-    margin : 13px 0px 13px 20px;
+    border: 1px solid rgb(206 212 217);
+    border-radius : 2px;
+    margin : -10px 0px 13px 20px;
     width : 80%;
-    height : 100%;
+    height : 270px;
+    display : flex;
+    flex-direction: row;
+}
+.subimage{
+    width : 100px;
+    height : 100px;
 }
 button {
-    height: 40%;
-    width : 300px;
-    background-color: black;
-
+    height: 80px;
+    width : 200px;
+    background-color: #49654E;
+    margin : 90px auto;
+    border-radius: 5px;
+    color: white;
+    font-family: 'Exo', sans-serif;
+    font-weight: 700;
+    font-size : 25px;
 }
 </style>
