7e8f7753e48ace07241fe862fa5fe158aad292f8
Jeong-Dagyeong <85853158+Jeong-Dagyeong@users.noreply.github.com>
2021-10-28 14:09:59 +0900

    20211028
    - 일단올려

---
+++ src/components/Admin_Brand_Insert.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="admin_brand_insert_wrapper">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style>
+.admin_brand_insert_wrapper {
+    height: 100%;
+    
+}
+
+</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -5,6 +5,7 @@ import Join from '@/components/Join.vue';
 import Login from '@/components/Login.vue';
 import Cart_Popup from '@/components/Cart_Popup.vue';
 import Admin_Page from '@/components/Admin_Page.vue';
+import Admin_Brand_Insert from '@/components/Admin_Brand_Insert.vue';
 
 
 const routes = [
@@ -13,6 +14,7 @@ const routes = [
     {path : '/login' , name :'login', component : Login},
     {path : '/cart_popup' , name :'cart_popup', component : Cart_Popup},
     {path : '/admin_page' , name :'admin_page', component : Admin_Page},
+    {path : '/admin_brand_insert' , name :'admin_brand_insert', component : Admin_Brand_Insert},
 ];
 
 const router = createRouter({
