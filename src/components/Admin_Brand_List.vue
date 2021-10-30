81c631f147334275fdda5051a53a9be737f4feda
Jeong-Dagyeong <85853158+Jeong-Dagyeong@users.noreply.github.com>
2021-10-31 01:55:02 +0900

    20211031_DK
    - 브랜드 등록현황 완료

--- src/components/Admin_Brand_List.vue
+++ src/components/Admin_Brand_List.vue
@@ -7,13 +7,8 @@
         </div>
         <hr class="solid" />
         <div class="adbr_list">
-            <table class="table table-borderless" style="">
-                <colgroup>
-                <col width="19%"/>
-                <col width="9%"/>
-                <col width="9%"/>
-                <col width="9%"/>
-                </colgroup>
+            <table class="table table-borderless" style="margin-left:50px;" >
+                <!-- style="margin-left:120px;" -->
                 <thead>
                     <tr>
                         <th scope="col"></th>
@@ -24,14 +19,18 @@
                 </thead>
                 <tbody>
                     <tr v-for="num in 10" v-bind:key="num">
-                        <td style="width:1px;">
-                        <div class="adbr_box" style="background:none" >
+                        <td style="width:1px; text-align: -webkit-center;">
+                        <div class="adbr_box">
                             <img :src="melixir" class="adbr_profile" >
                         </div>
                         </td>
-                        <td>Melixir</td>
-                        <td>2001.07.12</td>
-                        <td><button type="button" id="br_delete"> 삭제</button></td>
+                        <td style="padding: 25px 0;">Melixir</td>
+                        <td style="padding: 25px 0;">2001.07.12</td>
+                        <td>
+                        <button type="button" style="margin: 15px 0; width: 50px; border-radius: 7px;
+                        margin: 15px 0px; color: white; background-color: #49654E; border:none;">삭제</button>
+                        </td>
+                        <!-- id="br_delete" -->
                     </tr>
                 </tbody>
             </table>
@@ -70,18 +69,17 @@ import melixir from '@/assets/melixir.png';
 @import url('https://fonts.googleapis.com/css2?family=Exo:wght@300;400;500;600;700;800&display=swap');
 .table {
     font-family: 'Gowun Dodum', sans-serif;
+    text-align: center;
 }
 .admin_brand_list_wrapper {
     height: 100%;
     display : grid;
     grid-template-rows: 14% 1% 85%;
     grid-template-columns: 100%;
-    grid-template-areas: "br_in_header", "apl_divider" "apl_content";
     font-family: 'Exo', sans-serif;
 }
 .br_in_header {
     width: 100%;
-    /* border: 1px solid black; */
     display: flex;
     align-items: center;
     justify-content: flex-start;
@@ -110,27 +108,26 @@ import melixir from '@/assets/melixir.png';
     margin-top: 7px;
 }
 .adbr_list {
-    padding: 50px 0 0 150px;
-    text-align: center;
+    padding: 80px 300px 300px 300px;
+}
+tbody td {
+    padding: 25px 0;
 }
 .br_delete {
-   /* border: 1px solid black;  */
-   display: inline-block;
-   width: 50px;
-   height: 20px;
-   border-radius: 4px;
-   background-color: white;
-   border:0.1px;
+    width: 50px;
+    border-radius: 7px;
+    margin: 15px 0px;
+    color: white;
 }
 .adbr_box {
-    border: 1px solid black; 
+    /* border: 1px solid black;  */
     width: 70px;
     height: 70px; 
     border-radius: 70%;
     overflow: hidden;
 }
 .adbr_profile {
-    border: 1px solid black; 
+    /* border: 1px solid black;  */
     width: 100%;
     height: 100%;
     object-fit: cover;
